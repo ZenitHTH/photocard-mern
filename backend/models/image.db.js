aa817bb40c033c0514947002d0629cae3b608e63
settawat boriruklert <z.z.z.z.z.z.z.zenith@gmail.com>
2022-08-10 18:44:59 +0000

    change database

--- backend/models/image.db.js
+++ backend/models/image.db.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const imageSchema = new Schema({
-    name:String,
-    img:{data:Buffer,contentType:String}
-})
+  name: String,
+  title:String
+});
 
 module.exports = mongoose.model("imageSchema",imageSchema);
